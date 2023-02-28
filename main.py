9eb998a6178f89dba853b097809feb8db2a7b489
jasminep123 <jasminepotts225@gmail.com>
2023-02-28 16:32:05 +0000

    counts the number of sequences

--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import os
 #Reads the FASTQ file
 def readFile(path):
     verifyFile(path)
-    sequences = []
-    qualities = []
+    lines = []
     filename = os.path.basename(path)
     with open(filename) as fh:
         while True:
-            fh.readline() # skip name line
-            seq = fh.readline().rstrip() # read base sequence
-            if len(seq) == 0:
+            line = fh.readline()# read base sequence
+            if len(line) == 0:
                 break
-            sequences.append(seq)
-    return sequences
+            lines.append(line)
+    return lines
 
 # checks that the path entered exists and it is a valid FASTQ file
 def verifyFile(path):
@@ -24,8 +22,9 @@ def verifyFile(path):
         print("Not correct file type")
 
 # Counts the number of sequences in a file
-def countSeq():
-    #TODO count number of sequences
+def countSeq(lines):
+    numSeq = len(lines)/4
+    print("number of Sequences: ", numSeq)
     return
 
 # counts the number of nucleotides in a file
@@ -41,12 +40,13 @@ def gzip():
 if __name__ == '__main__':
     print("Welcome to the FASTQ file reader \n")
     path = input("Enter the file path: ")
-    readFile(path)
+    lines = readFile(path)
     print("To count the number of sequences press 'S' \n")
     print("To count the number of nucleotides press 'N' \n")
     request = input()
     if request == 'S' or request == 's':
         print("Counting the number of sequences ... \n")
+        countSeq(lines)
     elif request == 'n' or request == 'N':
         print("Counting the number of nucleotides ... \n")
 
